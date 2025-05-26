448edc9cb41a25d4ce68fed4e74c9acdabdae3ca
Alaksion <lbeurmann.dev@gmail.com>
2025-05-26 14:19:39 -0300

    feat: implement analytics

--- invoicer/invoicer/Src/Implementations/IosAnalyticsTracker.swift
+++ invoicer/invoicer/Src/Implementations/IosAnalyticsTracker.swift
@@ -6,9 +6,13 @@
 //
 
 import invoicerShared
+import FirebaseAnalytics
 
 final class IosAnalyticsTracker: AnalyticsTracker {
     func track(event: invoicerShared.AnalyticsEvent) {
-        
+        Analytics.logEvent(
+            event.name,
+            parameters: event.params
+        )
     }
 }
