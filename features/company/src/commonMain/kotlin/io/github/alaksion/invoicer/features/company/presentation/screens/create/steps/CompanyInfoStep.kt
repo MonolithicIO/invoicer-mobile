4113311ee704adc390d8325620ee18575cb3d33c
Alaksion <lbeurmann.dev@gmail.com>
2025-06-10 00:12:50 -0300

    feat: scaffold flow

--- features/company/src/commonMain/kotlin/io/github/alaksion/invoicer/features/company/presentation/screens/create/steps/CompanyInfoStep.kt
+++ features/company/src/commonMain/kotlin/io/github/alaksion/invoicer/features/company/presentation/screens/create/steps/CompanyInfoStep.kt
@@ -1,5 +1,6 @@
 package io.github.alaksion.invoicer.features.company.presentation.screens.create.steps
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
 
@@ -8,6 +9,15 @@ internal class CompanyInfoStep : Screen {
 
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+
+    }
+
+    @Composable
+    fun StateContent(
+
+    ) {
+        Scaffold {
+
+        }
     }
 }
