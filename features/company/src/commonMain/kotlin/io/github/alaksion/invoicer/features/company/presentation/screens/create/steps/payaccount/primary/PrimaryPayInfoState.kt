9d3c63379b09c8aaa638fe85d55ac44ce2e8c2bc
Alaksion <lbeurmann.dev@gmail.com>
2025-06-12 18:13:22 -0300

    feat: implement screen state

--- features/company/src/commonMain/kotlin/io/github/alaksion/invoicer/features/company/presentation/screens/create/steps/payaccount/primary/PrimaryPayInfoState.kt
+++ features/company/src/commonMain/kotlin/io/github/alaksion/invoicer/features/company/presentation/screens/create/steps/payaccount/primary/PrimaryPayInfoState.kt
@@ -4,7 +4,8 @@ internal data class PrimaryPayInfoState(
     val iban: String = "",
     val swift: String = "",
     val bankName: String = "",
-    val bankAddress: String = ""
+    val bankAddress: String = "",
+    val shouldGoToIntermediary: Boolean = true,
 ) {
     val buttonEnabled: Boolean =
         iban.isNotBlank() &&
