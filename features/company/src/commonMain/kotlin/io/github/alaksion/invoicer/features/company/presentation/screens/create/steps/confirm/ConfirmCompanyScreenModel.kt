98787bd47452d3455b106bf8aa6945f63fffd8ff
Alaksion <lbeurmann.dev@gmail.com>
2025-06-16 10:35:59 -0300

    feat: implement confirmation state

--- features/company/src/commonMain/kotlin/io/github/alaksion/invoicer/features/company/presentation/screens/create/steps/confirm/ConfirmCompanyScreenModel.kt
+++ features/company/src/commonMain/kotlin/io/github/alaksion/invoicer/features/company/presentation/screens/create/steps/confirm/ConfirmCompanyScreenModel.kt
@@ -4,6 +4,7 @@ import cafe.adriel.voyager.core.model.ScreenModel
 import io.github.alaksion.invoicer.features.company.presentation.model.CreateCompanyForm
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 internal class ConfirmCompanyScreenModel(
     private val form: CreateCompanyForm
@@ -12,4 +13,24 @@ internal class ConfirmCompanyScreenModel(
     private val _state = MutableStateFlow(ConfirmCompanyState())
     val state = _state.asStateFlow()
 
+    fun resumeState() {
+        _state.update {
+            it.copy(
+                companyName = form.companyName,
+                companyDocument = form.companyDocument,
+                addressLine1 = form.addressLine1,
+                addressLine2 = form.addressLine2,
+                city = form.city,
+                state = form.state,
+                postalCode = form.postalCode,
+                countryCode = form.countryCode,
+                primaryPayAccount = form.primaryPayAccount,
+                intermediaryPayAccount = form.intermediaryPayAccount
+            )
+        }
+    }
+
+    fun enableButton() {
+        _state.update { it.copy(isButtonEnabled = true) }
+    }
 }
