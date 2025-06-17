2ed4406ad81542d01139013ba5ba5753fc344629
Alaksion <lbeurmann.dev@gmail.com>
2025-06-17 12:15:24 -0300

    feat: remove module and gradle imports

--- features/beneficiary/services/src/commonMain/kotlin/io/github/alaksion/invoicer/features/beneficiary/services/domain/model/BeneficiariesModel.kt
+++
@@ -1,7 +0,0 @@
-package io.github.alaksion.invoicer.features.beneficiary.services.domain.model
-
-data class BeneficiariesModel(
-    val itemCount: Long,
-    val nextPage: Long?,
-    val items: List<BeneficiaryModel>,
-)
