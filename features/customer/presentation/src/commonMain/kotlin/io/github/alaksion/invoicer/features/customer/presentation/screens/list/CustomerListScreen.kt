d35279ead35e13bd5a283998a45b680865660e03
Alaksion <lbeurmann.dev@gmail.com>
2025-06-20 14:21:07 -0300

    feat: scaffold screen

--- features/customer/presentation/src/commonMain/kotlin/io/github/alaksion/invoicer/features/customer/presentation/screens/list/CustomerListScreen.kt
+++ features/customer/presentation/src/commonMain/kotlin/io/github/alaksion/invoicer/features/customer/presentation/screens/list/CustomerListScreen.kt
@@ -1,17 +1,61 @@
 package io.github.alaksion.invoicer.features.customer.presentation.screens.list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.koinScreenModel
+import cafe.adriel.voyager.navigator.LocalNavigator
+import io.github.alaksion.invoicer.foundation.ui.FlowCollectEffect
 
 internal class CustomerListScreen : Screen {
 
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+        val screenModel = koinScreenModel<CustomerListScreenModel>()
+        val navigator = LocalNavigator.current
+        val state by screenModel.state.collectAsState()
+
+        val callBacks = remember {
+            Callbacks(
+                onBack = { navigator?.pop() },
+                requestNextPage = screenModel::nextPage
+            )
+        }
+
+        LaunchedEffect(Unit) {
+            screenModel.loadPage()
+        }
+
+        FlowCollectEffect(
+            screenModel.events, screenModel
+        ) {
+            when (it) {
+                is CustomerListEvent.NextPageFailed -> {
+                    // Handle next page failed event
+                }
+            }
+        }
+
+        StateContent(
+            state = state,
+            callbacks = callBacks
+        )
     }
 
     @Composable
-    fun StateContent() {
+    fun StateContent(
+        state: CustomerListState,
+        callbacks: Callbacks
+    ) {
 
     }
+
+
+    data class Callbacks(
+        val onBack: () -> Unit,
+        val requestNextPage: () -> Unit,
+    )
 }
