9afe0c4c7d9beed4cedf2eed35a9ebf3f6c3bcf6
Alaksion <lbeurmann.dev@gmail.com>
2025-06-20 11:23:15 -0300

    feat: implement screenModel

--- features/customer/services/src/commonMain/kotlin/io/github/alaksion/invoicer/features/customer/domain/model/CustomerListModel.kt
+++ features/customer/services/src/commonMain/kotlin/io/github/alaksion/invoicer/features/customer/domain/model/CustomerListModel.kt
@@ -2,13 +2,13 @@ package io.github.alaksion.invoicer.features.customer.domain.model
 
 import kotlinx.datetime.Instant
 
-internal data class CustomerListModel(
+data class CustomerListModel(
     val itemCount: Long,
     val nextPage: Long? = null,
     val items: List<CustomerListItemModel>
 )
 
-internal data class CustomerListItemModel(
+data class CustomerListItemModel(
     val id: String,
     val name: String,
     val email: String,
