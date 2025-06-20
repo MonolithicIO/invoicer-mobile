6be70decebac95654d2e7fd9348c18fa436afc3b
Alaksion <lbeurmann.dev@gmail.com>
2025-06-20 14:24:12 -0300

    ga .

--- features/customer/presentation/src/commonMain/kotlin/io/github/alaksion/invoicer/features/customer/presentation/screens/list/CustomerListScreen.kt
+++ features/customer/presentation/src/commonMain/kotlin/io/github/alaksion/invoicer/features/customer/presentation/screens/list/CustomerListScreen.kt
@@ -1,5 +1,12 @@
 package io.github.alaksion.invoicer.features.customer.presentation.screens.list
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -8,6 +15,8 @@ import androidx.compose.runtime.remember
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
+import io.github.alaksion.invoicer.features.customer.presentation.screens.create.CreateCustomerScreen
+import io.github.alaksion.invoicer.foundation.designSystem.components.buttons.BackButton
 import io.github.alaksion.invoicer.foundation.ui.FlowCollectEffect
 
 internal class CustomerListScreen : Screen {
@@ -21,7 +30,10 @@ internal class CustomerListScreen : Screen {
         val callBacks = remember {
             Callbacks(
                 onBack = { navigator?.pop() },
-                requestNextPage = screenModel::nextPage
+                requestNextPage = screenModel::nextPage,
+                onCreateNewCustomer = {
+                    navigator?.push(CreateCustomerScreen())
+                }
             )
         }
 
@@ -45,17 +57,46 @@ internal class CustomerListScreen : Screen {
         )
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun StateContent(
         state: CustomerListState,
         callbacks: Callbacks
     ) {
-
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {},
+                    navigationIcon = {
+                        BackButton(onBackClick = callbacks.onBack)
+                    }
+                )
+            },
+            floatingActionButton = {
+                if (state.mode == CustomerListMode.Content) {
+                    FloatingActionButton(
+                        onClick = callbacks.onCreateNewCustomer
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Add,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        ) {
+            when (state.mode) {
+                CustomerListMode.Content -> TODO()
+                CustomerListMode.Error -> TODO()
+                CustomerListMode.Loading -> TODO()
+            }
+        }
     }
 
 
     data class Callbacks(
         val onBack: () -> Unit,
         val requestNextPage: () -> Unit,
+        val onCreateNewCustomer: () -> Unit
     )
 }
