331a9696bc459350bda6d596f37986c7bd88559c
Alaksion <lbeurmann.dev@gmail.com>
2025-07-08 15:04:14 -0300

    feat: update settings screen UI

--- features/home/src/commonMain/kotlin/io/github/alaksion/features/home/presentation/tabs/settings/SettingsTab.kt
+++ features/home/src/commonMain/kotlin/io/github/alaksion/features/home/presentation/tabs/settings/SettingsTab.kt
@@ -1,16 +1,21 @@
 package io.github.alaksion.features.home.presentation.tabs.settings
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Logout
+import androidx.compose.material.icons.outlined.Business
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.QrCodeScanner
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -28,11 +33,12 @@ import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import invoicer.features.home.generated.resources.Res
 import invoicer.features.home.generated.resources.home_settings_authorization
+import invoicer.features.home.generated.resources.home_settings_company
 import invoicer.features.home.generated.resources.home_settings_label
+import invoicer.features.home.generated.resources.home_settings_profile
 import invoicer.features.home.generated.resources.home_settings_sign_out
 import io.github.alaksion.features.home.presentation.tabs.settings.components.SettingsItem
 import io.github.alaksion.features.home.presentation.tabs.settings.components.SignOutDialog
-import io.github.alaksion.invoicer.foundation.designSystem.components.spacer.Spacer
 import io.github.alaksion.invoicer.foundation.designSystem.tokens.Spacing
 import io.github.alaksion.invoicer.foundation.navigation.InvoicerScreen
 import org.jetbrains.compose.resources.stringResource
@@ -54,17 +60,21 @@ internal object SettingsTab : Tab {
         val viewModel = koinScreenModel<SettingsScreenModel>()
         var signOutDialogState by remember { mutableStateOf(false) }
 
-        val callBacks = rememberSettingsCallbacks(
-            onSignOut = { signOutDialogState = true },
-            onConfirmSignOut = {
-                signOutDialogState = false
-                viewModel.signOut()
-            },
-            onCancelSignOut = { signOutDialogState = false },
-            goToAuthorizations = {
-                navigator?.push(ScreenRegistry.get(InvoicerScreen.Authorization.Home))
-            }
-        )
+        val callBacks = remember {
+            Callbacks(
+                onSignOutClick = { signOutDialogState = true },
+                onConfirmSignOut = {
+                    signOutDialogState = false
+                    viewModel.signOut()
+                },
+                onCancelSignOut = { signOutDialogState = false },
+                onAuthorizationClick = {
+                    navigator?.push(ScreenRegistry.get(InvoicerScreen.Authorization.Home))
+                },
+                onProfileClick = {},
+                onCompanyClick = {},
+            )
+        }
 
         StateContent(
             callbacks = callBacks,
@@ -75,9 +85,10 @@ internal object SettingsTab : Tab {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun StateContent(
-        callbacks: SettingsCallbacks,
+        callbacks: Callbacks,
         isDialogVisible: Boolean,
     ) {
+        val scroll = rememberScrollState()
         Scaffold(
             modifier = Modifier.background(Color.Red),
             topBar = {
@@ -93,26 +104,37 @@ internal object SettingsTab : Tab {
                     .fillMaxSize()
                     .padding(it)
                     .padding(Spacing.medium)
+                    .verticalScroll(scroll),
+                verticalArrangement = Arrangement.spacedBy(Spacing.small)
             ) {
                 SettingsItem(
                     modifier = Modifier.fillMaxWidth(),
                     content = stringResource(Res.string.home_settings_authorization),
                     icon = Icons.Outlined.QrCodeScanner,
-                    onClick = callbacks.goToAuthorizations
+                    onClick = callbacks.onAuthorizationClick
                 )
-                Spacer(1f)
-                Button(
+                HorizontalDivider()
+                SettingsItem(
                     modifier = Modifier.fillMaxWidth(),
-                    onClick = callbacks.onRequestSignOut,
-                ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Outlined.Logout,
-                        contentDescription = null
-                    )
-                    Text(
-                        text = stringResource(Res.string.home_settings_sign_out)
-                    )
-                }
+                    content = stringResource(Res.string.home_settings_profile),
+                    icon = Icons.Outlined.Person,
+                    onClick = callbacks.onProfileClick
+                )
+                HorizontalDivider()
+                SettingsItem(
+                    modifier = Modifier.fillMaxWidth(),
+                    content = stringResource(Res.string.home_settings_company),
+                    icon = Icons.Outlined.Business,
+                    onClick = callbacks.onCompanyClick
+                )
+                HorizontalDivider()
+                SettingsItem(
+                    modifier = Modifier.fillMaxWidth(),
+                    content = stringResource(Res.string.home_settings_sign_out),
+                    icon = Icons.AutoMirrored.Outlined.Logout,
+                    iconTint = MaterialTheme.colorScheme.primary,
+                    onClick = callbacks.onSignOutClick
+                )
             }
         }
         SignOutDialog(
@@ -121,4 +143,13 @@ internal object SettingsTab : Tab {
             isVisible = isDialogVisible
         )
     }
+
+    internal data class Callbacks(
+        val onSignOutClick: () -> Unit,
+        val onConfirmSignOut: () -> Unit,
+        val onCancelSignOut: () -> Unit,
+        val onAuthorizationClick: () -> Unit,
+        val onProfileClick: () -> Unit,
+        val onCompanyClick: () -> Unit,
+    )
 }
