f10f940520962922e32567cc51d04510b679a574
Alaksion <lbeurmann.dev@gmail.com>
2025-09-09 21:42:38 -0300

    feat: scaffold first component

--- foundation/design-system/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/designSystem/ink/values/InkColorScheme.kt
+++ foundation/design-system/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/designSystem/ink/values/InkColorScheme.kt
@@ -1,21 +1,24 @@
 package io.github.monolithic.invoicer.foundation.designSystem.ink.values
 
-import androidx.compose.ui.graphics.Color
+import io.github.monolithic.invoicer.foundation.designSystem.ink.tokens.InkColor
 import io.github.monolithic.invoicer.foundation.designSystem.ink.tokens.InkColors
 
 
 data class InkColorScheme(
-    val background: Color
+    val background: InkColor,
+    val onBackground: InkColor,
 )
 
 internal fun lightInkColorScheme(): InkColorScheme {
     return InkColorScheme(
-        background = InkColors.BlackWhite.white
+        background = InkColors.BlackWhite.white,
+        onBackground = InkColors.GreyScale.grey900
     )
 }
 
 internal fun darkInkColorScheme(): InkColorScheme {
     return InkColorScheme(
-        background = InkColors.Dark.dark1
+        background = InkColors.Dark.dark1,
+        onBackground = InkColors.BlackWhite.white
     )
 }
