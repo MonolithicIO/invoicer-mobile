1ec376fc17edd60e8df7326c135f314628cd0277
Alaksion <lbeurmann.dev@gmail.com>
2025-09-19 19:16:48 -0300

    feat: fix bottom bar paddings

--- features/auth/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/auth/presentation/screens/login/LoginScreen.kt
+++ features/auth/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/auth/presentation/screens/login/LoginScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -45,6 +44,7 @@ import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.compon
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.topbar.InkTopBar
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.theme.InkTheme
 import io.github.monolithic.invoicer.foundation.navigation.extensions.pushToFront
+import io.github.monolithic.invoicer.foundation.utils.modifier.systemBarBottomPadding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
@@ -126,8 +126,8 @@ internal class LoginScreen : Screen {
                     onClick = callBacks.onSubmit,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(InkTheme.spacing.medium)
-                        .systemBarsPadding()
+                        .padding(horizontal = InkTheme.spacing.medium)
+                        .systemBarBottomPadding()
                 )
             },
             snackBarHost = {
--- features/auth/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/auth/presentation/screens/signup/SignUpScreen.kt
+++ features/auth/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/auth/presentation/screens/signup/SignUpScreen.kt
@@ -1,12 +1,14 @@
 package io.github.monolithic.invoicer.features.auth.presentation.screens.signup
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -49,6 +52,7 @@ import io.github.monolithic.invoicer.foundation.designSystem.legacy.components.D
 import io.github.monolithic.invoicer.foundation.designSystem.legacy.components.InvoicerDialog
 import io.github.monolithic.invoicer.foundation.designSystem.legacy.components.spacer.SpacerSize
 import io.github.monolithic.invoicer.foundation.designSystem.legacy.components.spacer.VerticalSpacer
+import io.github.monolithic.invoicer.foundation.utils.modifier.systemBarBottomPadding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
@@ -151,7 +155,7 @@ internal class SignUpScreen : Screen {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = InkTheme.spacing.medium)
-                        .systemBarsPadding()
+                        .systemBarBottomPadding()
                 )
             }
         ) { scaffoldPadding ->
