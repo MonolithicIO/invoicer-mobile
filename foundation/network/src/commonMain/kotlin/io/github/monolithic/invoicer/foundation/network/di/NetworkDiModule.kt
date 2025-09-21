d6c090e6205f18b837d96ad6a95821e2d8e29723
Alaksion <lbeurmann.dev@gmail.com>
2025-09-21 19:46:03 -0300

    feat: simplify http client creation logic

--- foundation/network/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/network/di/NetworkDiModule.kt
+++ foundation/network/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/network/di/NetworkDiModule.kt
@@ -1,9 +1,18 @@
 package io.github.monolithic.invoicer.foundation.network.di
 
-import io.github.monolithic.invoicer.foundation.network.client.NetworkEngine
+import io.github.monolithic.invoicer.foundation.network.client.HttpClientProvider
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
 val networkDiModule = module {
-    single<HttpClient> { NetworkEngine }
+    single<HttpClient> {
+        val provider = get<HttpClientProvider>()
+        provider.provideClient()
+    }
+
+    factory {
+        HttpClientProvider(
+            authTokenRepository = get()
+        )
+    }
 }
