0684878719fce79e83faffbb61834fb1944ba347
Alaksion <lbeurmann.dev@gmail.com>
2025-09-23 00:19:00 -0300

    feat: add splash dimiss behavior

--- multiplatform/features/auth/src/commonMain/kotlin/io/github/monolithic/invoicer/features/auth/presentation/screens/startup/StartupScreenModel.kt
+++ multiplatform/features/auth/src/commonMain/kotlin/io/github/monolithic/invoicer/features/auth/presentation/screens/startup/StartupScreenModel.kt
@@ -4,6 +4,7 @@ import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import io.github.monolithic.invoicer.features.auth.domain.model.StartupDestination
 import io.github.monolithic.invoicer.features.auth.domain.services.ResolveStartupDestinationService
+import io.github.monolithic.invoicer.foundation.platform.splashdismisser.SplashScreenDismisser
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -11,7 +12,8 @@ import kotlinx.coroutines.launch
 
 internal class StartupScreenModel(
     private val startupDestinationService: ResolveStartupDestinationService,
-    private val dispatcher: CoroutineDispatcher
+    private val dispatcher: CoroutineDispatcher,
+    private val splashScreenDismisser: SplashScreenDismisser
 ) : ScreenModel {
 
     // TODO There seems to be a race condition in the UI Layer so replay here serves to solve this.
@@ -23,5 +25,9 @@ internal class StartupScreenModel(
             val destination = startupDestinationService.resolveDestination()
             _events.emit(destination)
         }
+    }
+
+    fun dismissSplashScreen() {
+        splashScreenDismisser.dismiss()
     }
 }
