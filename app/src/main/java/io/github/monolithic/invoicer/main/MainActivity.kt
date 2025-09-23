4857d661e93db6ca73b35c9bc4a163a42971076c
Alaksion <lbeurmann.dev@gmail.com>
2025-09-23 00:14:36 -0300

    feat: implement splash dismiss

--- app/src/main/java/io/github/monolithic/invoicer/main/MainActivity.kt
+++ app/src/main/java/io/github/monolithic/invoicer/main/MainActivity.kt
@@ -4,15 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import io.github.monolithic.invoicer.foundation.platform.splashdismisser.SplashScreenDismisser
+import io.github.monolithic.invoicer.foundation.platform.splashdismisser.SplashScreenDismisserDelegate
 import io.github.monolithic.invoicer.sharedApp.InvoicerApp
+import org.koin.android.ext.android.inject
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SplashScreenDismisserDelegate {
+    private val splashScreenDismisser by inject<SplashScreenDismisser>()
+
+    private var showSplashScreen = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        splashScreenDismisser.attachDelegate(this)
+        installSplashScreen().setKeepOnScreenCondition {
+            showSplashScreen
+        }
         setContent {
             InvoicerApp()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        splashScreenDismisser.detachDelegate()
+    }
+
+    override fun dismissSplashScreen() {
+        showSplashScreen = false
     }
 }
