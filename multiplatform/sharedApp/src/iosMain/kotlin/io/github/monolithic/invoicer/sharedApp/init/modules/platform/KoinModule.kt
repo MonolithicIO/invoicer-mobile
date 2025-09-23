3cb7042aff9faef85693a79bca35465ee50a503b
Alaksion <lbeurmann.dev@gmail.com>
2025-09-22 23:53:44 -0300

    feat: IOS - update initializer imports

--- multiplatform/sharedApp/src/iosMain/kotlin/io/github/monolithic/invoicer/sharedApp/init/modules/platform/KoinModule.kt
+++ multiplatform/sharedApp/src/iosMain/kotlin/io/github/monolithic/invoicer/sharedApp/init/modules/platform/KoinModule.kt
@@ -3,6 +3,7 @@ package io.github.monolithic.invoicer.sharedApp.init.modules.platform
 import io.github.monolithic.invoicer.foundation.auth.firebase.FirebaseHelper
 import io.github.monolithic.invoicer.foundation.auth.firebase.IosGoogleFirebaseHelper
 import io.github.monolithic.invoicer.foundation.platform.analytics.AnalyticsTracker
+import io.github.monolithic.invoicer.foundation.platform.storage.LocalStorage
 import io.github.monolithic.invoicer.sharedApp.init.di.appModule
 import io.github.monolithic.invoicer.sharedApp.init.modules.ModuleInitializer
 import org.koin.core.context.startKoin
