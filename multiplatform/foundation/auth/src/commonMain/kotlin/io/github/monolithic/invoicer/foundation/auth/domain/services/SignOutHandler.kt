088773201d0c706eab7fb40dbb19fd3486f2b88c
Alaksion <lbeurmann.dev@gmail.com>
2025-09-28 18:08:42 -0300

    feat: clear user storage on sign-out

--- multiplatform/foundation/auth/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/auth/domain/services/SignOutHandler.kt
+++ multiplatform/foundation/auth/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/auth/domain/services/SignOutHandler.kt
@@ -2,6 +2,8 @@ package io.github.monolithic.invoicer.foundation.auth.domain.services
 
 import io.github.monolithic.invoicer.foundation.auth.domain.repository.AuthRepository
 import io.github.monolithic.invoicer.foundation.auth.firebase.FirebaseHelper
+import io.github.monolithic.invoicer.foundation.platform.storage.LocalStorage
+import io.github.monolithic.invoicer.foundation.platform.storage.StorageKeys
 import io.github.monolithic.invoicer.foundation.watchers.bus.feature.AuthEvent
 import io.github.monolithic.invoicer.foundation.watchers.bus.feature.AuthEventBus
 
@@ -13,10 +15,16 @@ internal class SignOutHandler(
     private val authRepository: AuthRepository,
     private val authEventBus: AuthEventBus,
     private val firebaseHelper: FirebaseHelper,
+    private val localStorage: LocalStorage
 ) : SignOutService {
     override suspend fun signOut() {
         authRepository.clearAuthTokens()
         firebaseHelper.signOut()
+        clearUserStorage()
         authEventBus.publishEvent(AuthEvent.SignedOut)
+    }
+
+    private fun clearUserStorage() {
+        localStorage.clear(StorageKeys.SelectedCompany.key)
     }
 }
---
+++ multiplatform/foundation/platform/src/commonMain/kotlin/io/github/monolithic/invoicer/foundation/platform/storage/StorageKeys.kt
@@ -0,0 +1,5 @@
+package io.github.monolithic.invoicer.foundation.platform.storage
+
+enum class StorageKeys(val key: String) {
+    SelectedCompany("selected_company_id")
+}
