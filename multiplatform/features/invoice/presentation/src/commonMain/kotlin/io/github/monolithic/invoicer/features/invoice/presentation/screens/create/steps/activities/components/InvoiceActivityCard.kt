c9c674ccb965ebc7793cd4a3341de8ca791abdf7
Alaksion <lbeurmann.dev@gmail.com>
2025-10-02 20:36:16 -0300

    feat: redesign activity items

--- multiplatform/features/invoice/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/invoice/presentation/screens/create/steps/activities/components/InvoiceActivityCard.kt
+++ multiplatform/features/invoice/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/invoice/presentation/screens/create/steps/activities/components/InvoiceActivityCard.kt
@@ -15,10 +15,12 @@ import androidx.compose.ui.text.style.TextOverflow
 import invoicer.multiplatform.features.invoice.presentation.generated.resources.Res
 import invoicer.multiplatform.features.invoice.presentation.generated.resources.invoice_create_activity_item_quantity
 import invoicer.multiplatform.features.invoice.presentation.generated.resources.invoice_create_activity_item_total_paid
+import invoicer.multiplatform.features.invoice.presentation.generated.resources.invoice_create_activity_item_unit_price
 import io.github.monolithic.invoicer.features.invoice.presentation.screens.create.steps.activities.model.CreateInvoiceActivityUiModel
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.InkCard
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.InkText
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.InkTextStyle
+import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.Spacer
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.divider.InkHorizontalDivider
 import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.theme.InkTheme
 import io.github.monolithic.invoicer.foundation.utils.money.moneyFormat
@@ -40,62 +42,30 @@ internal fun InvoiceActivityCard(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(InkTheme.spacing.medium)
         ) {
-            InvoiceActivityHeader(
-                modifier = Modifier.fillMaxWidth(),
-                description = item.description,
-                quantity = item.quantity.toString(),
-                unitPrice = item.unitPrice.moneyFormat(),
-            )
-            InkHorizontalDivider(
-                color = InkTheme.colorScheme.onSurface
-            )
-            InvoiceActivityFooter(
-                totalPrice = item.totalPrice.moneyFormat()
-            )
-        }
-
-    }
-}
-
-@Composable
-private fun InvoiceActivityHeader(
-    description: String,
-    quantity: String,
-    unitPrice: String,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(InkTheme.spacing.medium)
-    ) {
-        Column(
-            modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.spacedBy(InkTheme.spacing.xSmall3)
-        ) {
             InkText(
-                text = description,
+                text = item.description,
                 style = InkTextStyle.Heading5,
                 weight = FontWeight.SemiBold,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1
             )
-            InkText(
-                text = stringResource(
-                    Res.string.invoice_create_activity_item_quantity,
-                    quantity
-                ),
-                color = InkTheme.colorScheme.onBackgroundVariant,
-                style = InkTextStyle.BodyLarge,
+            InkHorizontalDivider(color = InkTheme.colorScheme.onSurface)
+            ActivityItemRow(
+                label = stringResource(Res.string.invoice_create_activity_item_quantity),
+                value = item.quantity.toString()
             )
+            InkHorizontalDivider(color = InkTheme.colorScheme.onSurface)
+            ActivityItemRow(
+                label = stringResource(Res.string.invoice_create_activity_item_unit_price),
+                value = item.unitPrice.moneyFormat()
+            )
+            InkHorizontalDivider(color = InkTheme.colorScheme.onSurface)
+            InvoiceActivityFooter(totalPrice = item.totalPrice.moneyFormat())
         }
-        InkText(
-            text = unitPrice,
-            style = InkTextStyle.Heading5,
-            weight = FontWeight.Bold,
-        )
+
     }
 }
+
 
 @Composable
 private fun InvoiceActivityFooter(
@@ -117,6 +87,31 @@ private fun InvoiceActivityFooter(
             style = InkTextStyle.Heading5,
             weight = FontWeight.Black,
             color = InkTheme.colorScheme.primaryVariant
+        )
+    }
+}
+
+@Composable
+private fun ActivityItemRow(
+    label: String,
+    value: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(InkTheme.spacing.medium)
+    ) {
+        InkText(
+            text = label,
+            weight = FontWeight.SemiBold,
+            style = InkTextStyle.Heading6
+        )
+        Spacer(1f)
+        InkText(
+            text = value,
+            weight = FontWeight.Bold,
+            style = InkTextStyle.Heading6
         )
     }
 }
