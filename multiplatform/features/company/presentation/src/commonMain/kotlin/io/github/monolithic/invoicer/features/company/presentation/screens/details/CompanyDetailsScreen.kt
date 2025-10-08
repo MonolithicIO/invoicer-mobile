942b7d2a54fa3514df5484bc1441b525a37ad81a
Alaksion <lbeurmann.dev@gmail.com>
2025-10-08 20:53:59 -0300

    lint

--- multiplatform/features/company/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/company/presentation/screens/details/CompanyDetailsScreen.kt
+++ multiplatform/features/company/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/company/presentation/screens/details/CompanyDetailsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -97,7 +96,6 @@ internal class CompanyDetailsScreen : Screen {
         )
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun StateContent(
         state: CompanyDetailsState,
