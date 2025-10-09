016d3d8556427df0504a457a0204a34f4be3c50b
Alaksion <lbeurmann.dev@gmail.com>
2025-10-08 21:58:49 -0300

    feat: update delete account modal

--- multiplatform/features/company/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/company/presentation/screens/updatepayaccount/components/DeletePayAccountModal.kt
+++ multiplatform/features/company/presentation/src/commonMain/kotlin/io/github/monolithic/invoicer/features/company/presentation/screens/updatepayaccount/components/DeletePayAccountModal.kt
@@ -1,14 +1,13 @@
 package io.github.monolithic.invoicer.features.company.presentation.screens.updatepayaccount.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DeleteOutline
 import androidx.compose.runtime.Composable
 import invoicer.multiplatform.features.company.presentation.generated.resources.Res
 import invoicer.multiplatform.features.company.presentation.generated.resources.delete_pay_account_cancel
 import invoicer.multiplatform.features.company.presentation.generated.resources.delete_pay_account_confirm
 import invoicer.multiplatform.features.company.presentation.generated.resources.delete_pay_account_description
 import invoicer.multiplatform.features.company.presentation.generated.resources.delete_pay_account_title
-import io.github.monolithic.invoicer.foundation.designSystem.legacy.components.dialog.DefaultInvoicerDialog
+import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.dialog.InkDialog
+import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.dialog.props.InkDialogAction
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -18,14 +17,18 @@ internal fun DeletePayAccountModal(
     isVisible: Boolean,
 ) {
     if (isVisible) {
-        DefaultInvoicerDialog(
-            onDismiss = onDismissRequest,
-            icon = Icons.Outlined.DeleteOutline,
+        InkDialog(
+            onDismissRequest = onDismissRequest,
             title = stringResource(Res.string.delete_pay_account_title),
             description = stringResource(Res.string.delete_pay_account_description),
-            confirmButtonText = stringResource(Res.string.delete_pay_account_confirm),
-            confirmButtonClick = onConfirm,
-            cancelButtonText = stringResource(Res.string.delete_pay_account_cancel)
+            primaryAction = InkDialogAction(
+                title = stringResource(Res.string.delete_pay_account_confirm),
+                onClick = onConfirm
+            ),
+            secondaryAction = InkDialogAction(
+                title = stringResource(Res.string.delete_pay_account_cancel),
+                onClick = onDismissRequest
+            ),
         )
     }
 }
