27daa4f1b60a13cf38f1eedd79962c8f8469d0e6
Alaksion <lbeurmann.dev@gmail.com>
2025-10-19 17:08:24 -0300

    feat: implement otp input

---
+++ multiplatform/foundation/design-system/src/androidMain/kotlin/io/github/monolithic/invoicer/foundation/designSystem/ink/previews/OtpInput.kt
@@ -0,0 +1,42 @@
+package io.github.monolithic.invoicer.foundation.designSystem.ink.previews
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.monolithic.invoicer.foundation.designSystem.ink.PreviewContainer
+import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.button.InkPrimaryButton
+import io.github.monolithic.invoicer.foundation.designSystem.ink.internal.components.input.InkOtpInput
+
+@Composable
+@Preview
+private fun OtpInputPreview() {
+    PreviewContainer(true) {
+        var state by remember { mutableStateOf("") }
+        var error by remember { mutableStateOf(false) }
+
+        LaunchedEffect(state) {
+            println("state: $state")
+        }
+
+        InkOtpInput(
+            value = state,
+            digitCount = 2,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {
+                state = it.trim()
+            },
+            isError = error
+        )
+
+        InkPrimaryButton(
+            text = "Toggle Error",
+            modifier = Modifier.fillMaxWidth()
+        ) { error = error.not() }
+    }
+}
